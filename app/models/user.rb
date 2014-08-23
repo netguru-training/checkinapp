f8d5955ee77234ae209fba86c47cde1cec88007a
sneaky22 <jsamol22@wp.pl>
2014-08-23 15:37:29 +0200

    Finding firends for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :checkins
+
+  def friends
+    User.find((Friendship.senders(self.id).pluck(:receiver_id)+Friendship.receivers(self.id).pluck(:sender_id)).uniq)
+  end
 end
