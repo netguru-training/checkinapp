d9f52d5bf761b2b920a18578e299c6f59907972c
Adam <vexhelel@gmail.com>
2014-08-23 11:18:58 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823083719) do
+ActiveRecord::Schema.define(version: 20140823084321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "checkins", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "timestamp"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.text     "street"
+    t.string   "city"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
