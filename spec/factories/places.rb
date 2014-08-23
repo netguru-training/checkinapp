ab24e63ef4b4ebbb87247a32684664d14dab400f
Adam <vexhelel@gmail.com>
2014-08-23 15:45:23 +0200

    PlacesController filled

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  street     :text
+#  city       :string(255)
+#  country    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  postcode   :string(255)
+#
+
 FactoryGirl.define do
   factory :place do
     name "MyString"
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,6 +2,16 @@
 #
 # Table name: places
 #
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  street     :text
+#  city       :string(255)
+#  country    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  postcode   :string(255)
+#
+
 <<<<<<< HEAD
 #  id         :integer          not null, primary key
 #  name       :string(255)
