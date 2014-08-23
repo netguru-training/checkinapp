5ebbafc2cd3e10d20f6a9eb54a4245c395109618
azranel <chunkyo@gmail.com>
2014-08-23 15:48:29 +0200

    annotate for places

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -2,15 +2,16 @@
 #
 # Table name: places
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  street      :text
-#  city        :string(255)
-#  country     :string(255)
-#  created_at  :datetime
-#  updated_at  :datetime
-#  postcode    :string(255)
-#  coordinates :string(255)
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  street     :text
+#  city       :string(255)
+#  country    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  postcode   :string(255)
+#  longitude  :float            not null
+#  latitude   :float            not null
 #
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
