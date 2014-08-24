d3e347c9dcc824ab01954f8e433ff7f4952442a8
Adam <vexhelel@gmail.com>
2014-08-24 14:38:56 +0200

    fix friends near

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,11 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in? and !current_user.last_checkin.blank?
-      @x = current_user.checkins.last.place.longitude
-      @y = current_user.checkins.last.place.latitude
-      @friends_near = Array.new
-      @places = Place.near([@y, @x], 200, :units => :km)
-      @friends = current_user.friends
-      @friends.each do |f|
-        @places.each do |p|
-        if f.checkins.last.place.id = p.id
-          @friends_near.push(f)
-        end
-      end
+      last_place = current_user.last_checkin.place
+      places = Place.near([last_place.latitude, last_place.longitude], 1, units: :km)
+
+      friend_checkins = current_user.friends.map(&:last_checkin).compact
+      @checkins = friend_checkins.select { |c| places.include?(c.place) }
     end
-  end
   end
 end
