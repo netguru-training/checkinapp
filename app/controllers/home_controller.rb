8024122d4a336159c9d36432342ca1a9959f1627
Adam <vexhelel@gmail.com>
2014-08-24 15:17:09 +0200

    hack for km params

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   def index
     if user_signed_in? and !current_user.last_checkin.blank?
       last_place = current_user.last_checkin.place
-      places = Place.near([last_place.latitude, last_place.longitude], 1, units: :km)
+      km = params[:km] || 1
+      places = Place.near([last_place.latitude, last_place.longitude], km, units: :km)
 
       friend_checkins = current_user.friends.map(&:last_checkin).compact
       checkins = friend_checkins.select { |c| places.include?(c.place) }
