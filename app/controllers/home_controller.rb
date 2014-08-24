5f75f1d539f26eee4cdbcdddec153ff74107c015
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2014-08-24 16:27:18 +0200

    fix to many markers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,8 @@ class HomeController < ApplicationController
       places = Place.near([last_place.latitude, last_place.longitude], km, units: :km)
 
       friend_checkins = current_user.friends.map(&:last_checkin).compact
-      gon.markers = friend_checkins.map(&:place).map { |p| [p.latitude, p.longitude] }
       checkins = friend_checkins.select { |c| places.include?(c.place) }
+      gon.markers = checkins.map(&:place).map { |p| [p.latitude, p.longitude] }
       @checkins = CheckinDecorator.decorate_collection(checkins)
     end
   end
