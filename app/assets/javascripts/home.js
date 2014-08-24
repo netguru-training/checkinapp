0d5060a4647fbdbfb08a4932db564b56e24b1f9d
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2014-08-24 15:51:46 +0200

    display markers on map

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  if ($('.home.index').length == 0) { return }
+
+  var mapOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(52.4068254, 16.9270717)
+  }
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  if (gon.markers.length == 0) { return }
+  for(var i = 0; i < gon.marks.length; i++) {
+    var position = new google.maps.LatLng(gon.markers[i][0], gon.markers[i][0]);
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+  }
+});
